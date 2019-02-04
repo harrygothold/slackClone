82e4418318fb1f5fc5522b2da67e998788abef90
Harry Gothold <your.name@kurtgeiger.comgit config --global user.email harry.gothold@kurtgeiger.com>
2019-02-04 09:21:50 +0000

    finished user panel dropdown

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,41 +1,69 @@
-import React from 'react';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
+import React from "react";
+import firebase from "../../firebase";
+import { Grid, Header, Icon, Dropdown, Image } from "semantic-ui-react";
 
 class UserPanel extends React.Component {
+
+    state = {
+        user: this.props.currentUser,
+    }
+
     dropdownOptions = () => [
         {
-            key: 'user',
-            text: <span>Signed in as <strong>User</strong></span>,
+            key: "user",
+            text: (
+                <span>
+                    Signed in as <strong>{this.state.user.displayName}</strong>
+                </span>
+            ),
             disabled: true
         },
         {
-            key: 'avatar',
+            key: "avatar",
             text: <span>Change Avatar</span>
         },
         {
-            key: 'signout',
-            text: < span > Sign Out</span >
+            key: "signout",
+            text: <span onClick={this.handleSignout}>Sign Out</span>
         }
-    ]
+    ];
+
+    handleSignout = () => {
+        firebase
+            .auth()
+            .signOut()
+            .then(() => console.log("signed out!"));
+    };
+
     render() {
+        const { user } = this.state;
+
         return (
-            <Grid style={{ background: '#4c3c4c' }}>
+            <Grid style={{ background: "#4c3c4c" }}>
                 <Grid.Column>
-                    <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
-                        <Header inverted floated='left' as='h2'>
-                            <Icon name='code' />
+                    <Grid.Row style={{ padding: "1.2em", margin: 0 }}>
+                        {/* App Header */}
+                        <Header inverted floated="left" as="h2">
+                            <Icon name="code" />
                             <Header.Content>DevChat</Header.Content>
                         </Header>
+                        {/* User Dropdown  */}
+                        <Header style={{ padding: "0.25em" }} as="h4" inverted>
+                            <Dropdown
+                                trigger={<span>
+                                    <Image src={user.photoURL} spaced="right" avatar />
+                                    {user.displayName}
+                                </span>
+                                }
+                                options={this.dropdownOptions()}
+                            />
+                        </Header>
                     </Grid.Row>
-                    {/* User Dropdown */}
-                    <Header style={{ padding: '0.25rem' }} as='h4' inverted></Header>
-                    <Dropdown trigger={
-                        <span>User</span>
-                    } options={this.dropdownOptions()} />
                 </Grid.Column>
             </Grid>
-        )
+        );
     }
 }
+
 
 export default UserPanel;
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 import "semantic-ui-css/semantic.min.css";
 import rootReduer from './reducers';
-import { setUser } from './actions';
+import { setUser, clearUser } from './actions';
 import Spinner from './Spinner';
 
 const store = createStore(rootReduer, composeWithDevTools());
@@ -24,6 +24,9 @@ class Root extends React.Component {
             if (user) {
                 this.props.setUser(user);
                 this.props.history.push("/");
+            } else {
+                this.props.history.push('/login');
+                this.props.clearUser();
             }
         })
     }
@@ -43,7 +46,7 @@ const mapStateToProps = state => ({
     isLoading: state.user.isLoading
 });
 
-const RootWithAuth = withRouter(connect(mapStateToProps, { setUser })(Root));
+const RootWithAuth = withRouter(connect(mapStateToProps, { setUser, clearUser })(Root));
 
 ReactDOM.render(
     <Provider store={store}>
